eac97b57be926cbfaa643e7e38343dad9959c9d2
lolow <ldrouet@gmail.com>
2015-07-03 12:32:16 +0200

    add batch extract

--- R/gdxtools.R
+++ R/gdxtools.R
@@ -11,3 +11,24 @@ NULL
   # try to specify the gams installation
   igdx(dirname(Sys.which("gams")))
 }
+
+#' Extract a list of items from many GDX
+#'
+#' @export
+#' @param items vector or list of items to extract
+#' @param files list of files, if not defined gdxs should be defined
+#' @param gdxs list of gdxs, if not defined files should be defined
+batch_extract <- function(items,files=NULL,gdxs=NULL){
+  if(is.null(gdxs)){
+    gdxs = lapply(files, gdx)
+  }
+  lall = list()
+  for(item in items){
+    tt = lapply(gdxlist,extract,item,addgdx=T)
+    tt = do.call("rbind",tt)
+    tt = list(tt)
+    names(tt) <- item
+    lall <- c(lall,tt)
+  }
+  return(lall)
+}
