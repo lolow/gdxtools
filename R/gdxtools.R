092870191a8ca0088aca03e33091c8447a1da33e
Laurent Drouet <ldrouet@gmail.com>
2016-03-07 16:18:54 +0100

    add write gdx

--- R/gdxtools.R
+++ R/gdxtools.R
@@ -3,6 +3,7 @@
 #' @name gdxtools
 #' @docType package
 #' @import gdxrrw
+#'
 NULL
 
 .onAttach <- function(libname, pkgname) {
@@ -32,3 +33,61 @@ batch_extract <- function(items,files=NULL,gdxs=NULL){
   }
   return(lall)
 }
+
+#' Write a list of parameters in a gdx
+#'
+#' @export
+#' @param file the filename of the gdx to save
+#' @param params named list of parameters
+#' @param usetempdir uses system temp dir for the temporary files, otherwise use local file "tmp.gms"
+#' @param removeLST remove temporary lst file
+#' @examples
+#'  \dontrun{
+#'     param1 = data.frame(x=c('1','2'),value=1:10)
+#'     param2 = data.frame(a=c('london','paris','tahiti'),value=c(50,0.2,1e-2))
+#'     write_gdx("test.gdx",list(param1=param1,param2=param2))
+#'  }
+#'
+write_gdx <- function(file, params=list(), removeLST=T, usetempdir=T){
+  # Create a temporary gams file
+  if(usetempdir){
+    gms = tempfile(pattern = "wgdx", fileext = ".gms")
+    lst = tempfile(pattern = "wgdx", fileext = ".lst")
+  } else {
+    gms = "tmp.gms"
+    lst = "tmp.lst"
+  }
+  fgms = file(gms, "w")
+  # collect and write sets
+  allsets = unique(unlist(lapply(params, names)))
+  allsets = subset(allsets, !allsets %in% c('*'))
+  for(i in seq_along(allsets)){
+    s = allsets[i]
+    writeLines(paste("set", s, "/"), fgms)
+    lvalues = lapply(params, function(x) unique((as.character(x[[s]]))))
+    values = unique(unlist(lvalues))
+    writeLines(values, fgms)
+    writeLines("/;", fgms)
+  }
+  # Write parameters
+  for(i in seq_along(params)){
+    p = params[[i]]
+    if(length(colnames(p))==1){
+      writeLines(paste("scalar", names(params)[i], "/", as.numeric(p[1]), "/;"), fgms)
+    } else {
+      indices = subset(colnames(p), colnames(p) != "value")
+      writeLines(paste0("parameter ", names(params)[i],
+                        "(", paste(indices, collapse=","), ") /"), fgms)
+      concatenate <- function(row, len) paste(paste(row[1:len],collapse="."),row[len+1])
+      writeLines(apply(p,1,concatenate, len=length(indices)), fgms)
+      writeLines("/;", fgms)
+    }
+  }
+  # save into a gdx
+  writeLines(paste0('execute_unload "',filename,'"\n',paste(names(params),collapse="\n"),"\n;"), fgms)
+  close(fgms)
+  res = gams(paste0(gms," output=",lst))
+  if(res!=0) stop(paste("write gdx failed -",gms))
+  if(removeLST) file.remove(lst)
+}
+
